f64564345549b993055a6135b5afb7b0dd81e8ad
Valentyna Dumbrava <valyamoldovan55555@outlook.com>
2023-09-13 18:37:10 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,25 +5,37 @@ const form = document.querySelector('.feedback-form');
  const messagesInput = document.querySelector('textarea');
 //const { email, message } = form.elements;
 const LOCALSTORAGE_KEY = "feedback-form-state";
-
+updateOutput();
 form.addEventListener('input', throttle(writeForm, 500));
 const formData = {};
 function writeForm(event) {
     formData[event.target.name] = event.target.value;
-    console.log(formData);
+    //console.log(formData);
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
     
 }
 updateOutput();
 form.addEventListener("submit", saveMessage);
-function saveMessage(event){
+function saveMessage(event) {
+    if (emailInput.value === '' || messagesInput.value === '') {
+        return alert('Всі поля повинні бути заповнені');
+    }
     event.preventDefault();
-    updateOutput();
+    console.log(formData);
+    localStorage.removeItem(LOCALSTORAGE_KEY);
     form.reset();
+    
 }
 function updateOutput(event) {
     const saveFormData = localStorage.getItem(LOCALSTORAGE_KEY);
-    const parsedFormData = JSON.parse(saveFormData);   
-    emailInput.value = parsedFormData.email || "";
-    messagesInput.value = parsedFormData.message || "";
+    const parsedFormData = JSON.parse(saveFormData);  
+    if (parsedFormData) {
+        emailInput.value = parsedFormData.email;
+        messagesInput.value = parsedFormData.message;
+    }
+    else {
+        emailInput.value = '';
+    messagesInput.value = '';
+    }
+    
 }
